5e5e783481f2c24fc07ffe9a24d3b18a9cc97995
costa <costa@hellotalk.com>
2024-04-30 19:01:36 +0800

    feat: init code

--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -34,7 +34,58 @@ local function config()
   vim.g.go_fmt_command = "goimports" 
   -- 当格式化失败时，不报错。
  vim.g.go_fmt_fail_silently = 1
+ -- 开启gofmt实验模式
+ vim.g.go_fmt_experimental = 1
+ -- 键入shift-k时，在另一个窗口显示光标下word的godoc
+ vim.g.go_doc_keywordprg_enabled = 1
+ -- 使用浮动窗口显示word的godoc信息
+ vim.g.go_doc_popup_window = 1
+ -- Maximum height for the GoDoc window created with |:GoDoc|. Default is 20.
+ vim.g.go_doc_max_height = 20
+ -- Show GoDoc in balloon.
+ vim.g.go_doc_balloon = 0
+ -- Use this option to define the command to be used for |:GoFillStruct|. By
+ -- default `fillstruct` is used. Valid values are `fillstruct` and `gopls`. By
+ vim.g.go_fillstruct_mode = 'fillstruct'
+ 
 
+ vim.g.go_referrers_mode = 'gopls'
+
+
+ vim.g.go_implements_mode = 'gopls'
+ -- :GoDef所执行的命令，可选项：godef, gopls和guru。默认gopls，因为它很快
+ vim.g.go_def_mode = 'gopls'
+ -- 是否可以使用gopls，默认可以
+ vim.g.go_gopls_enabled = 1
+
+ -- Use this option to jump to an existing buffer for `:GoDef`, `:GoDefType`, and
+ vim.g.go_def_reuse_buffer = 1
+ -- The commandline arguments to pass to gopls.
+ -- By default, it is `['-remote=auto']`.
+ vim.g.go_gopls_options = ['-remote=auto']
+
+
+ -- 执行`:GoAddTags`时tag名字格式，这里设置为驼峰格式
+ vim.g.go_addtags_transform = 'camelcase'
+
+ -- 高亮函数和方法
+ vim.g.go_highlight_function_parameters = 1
+ vim.g.go_highlight_diagnostic_errors = 1
+ vim.g.go_highlight_diagnostic_warnings = 1
+ vim.g.go_highlight_string_spellcheck = 1
+ vim.g.go_highlight_format_strings = 1
+ vim.g.go_highlight_function_calls = 1
+ vim.g.go_highlight_methods = 1
+ vim.g.go_highlight_build_constraints = 1
+ vim.g.go_highlight_types = 1
+ vim.g.go_highlight_fields = 1
+ vim.g.go_highlight_functions = 1
+ vim.g.go_highlight_generate_tags = 1
+ vim.g.go_highlight_trailing_whitespace_error = 1
+ vim.g.go_highlight_array_whitespace_error = 1
+ vim.g.go_highlight_extra_types = 1
+ vim.g.go_highlight_space_tab_error = 1
+ vim.g.go_highlight_operators = 1
 end
 
 return {
